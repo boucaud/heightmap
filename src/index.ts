9aeba4dd4bd3bf670fd0dea129c7ea0a415fc528
Adrien Boucaud <adrien.bk@free.fr>
2022-02-25 21:17:25 +0100

    Add basic three.js scene

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,40 @@
-function component() {
-  const element = document.createElement("div");
+import {
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  BoxGeometry,
+  MeshBasicMaterial,
+  Mesh,
+} from "three";
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = "ok";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-  return element;
+const scene = new Scene();
+
+const camera = new PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new BoxGeometry();
+const material = new MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new Mesh(geometry, material);
+scene.add(cube);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+camera.position.z = 5;
+
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
 }
 
-document.body.appendChild(component());
+animate();
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,47 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: "./src/index.ts",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   devServer: {
-    static: './dist',
+    static: "./dist",
   },
   module: {
     rules: [
       {
         test: /\.(png|jpg|svg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
-    ]
+    ],
   },
   resolve: {
     alias: {
-      assets: path.resolve(__dirname, 'assets/'),
-      'boucaud-technical-test': path.resolve(__dirname, 'src/')
+      assets: path.resolve(__dirname, "assets/"),
+      "boucaud-technical-test": path.resolve(__dirname, "src/"),
     },
-    extensions: ['.tsx', '.ts', '.js'],
-  }
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  // See: https://github.com/mrdoob/three.js/issues/17482#issuecomment-780920930 TODO: unnecessary if no examples are used. Required for controls right now.
+  externals: [
+    ({ context, request }, callback) => {
+      if (request === "three" || request.endsWith("three.module.js")) {
+        return callback(null, {
+          commonjs: "three",
+          commonjs2: "three",
+          amd: "three",
+          root: "THREE",
+        });
+      }
+      callback();
+    },
+  ],
 };
-
