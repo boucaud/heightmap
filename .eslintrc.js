de625655be9d57fca1f8e7a2e0810a435d38a8ee
Adrien Boucaud <adrien.bk@free.fr>
2022-02-25 20:58:38 +0100

    Setup webpack project

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
+  env: {
+    es6: true,
+  },
+  extends: ["prettier"],
+};
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|svg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ]
+  },
+  resolve: {
+    alias: {
+      assets: path.resolve(__dirname, 'assets/'),
+      'boucaud-technical-test': path.resolve(__dirname, 'src/')
+    },
+    extensions: ['.tsx', '.ts', '.js'],
+  }
+};
+
