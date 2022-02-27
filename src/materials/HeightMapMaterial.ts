5aaf86a3840c419ed9f8f4318a0833b93b2b1ae0
Adrien Boucaud <adrien.bk@free.fr>
2022-02-27 02:06:04 +0100

    Add isolines to map

--- src/materials/HeightMapMaterial.ts
+++ src/materials/HeightMapMaterial.ts
@@ -1,15 +1,16 @@
-import { ShaderMaterial, DoubleSide, Texture } from "three";
+import { ShaderMaterial, DoubleSide, Texture, Color } from "three";
 
 import { textures } from "../TextureManager";
 
 const vs = `
   varying vec2 vUV;
+  varying float height;
   uniform float gridLength;
   uniform sampler2D heightMap;
   uniform float heightScaleFactor;
   void main() {
     vUV = vec2(position.x / gridLength, position.y / gridLength) + vec2(0.5, 0.5);
-    float height = texture2D(heightMap, vUV).x * heightScaleFactor;
+    height = texture2D(heightMap, vUV).x * heightScaleFactor;
     vec4 bumpedPosition = vec4(position.xy, height, 1.0);
     gl_Position = projectionMatrix * modelViewMatrix * bumpedPosition;
   }
@@ -17,11 +18,28 @@ const vs = `
 
 const fs = `
   varying vec2 vUV;
+  varying float height;
   uniform float gridLength;
   uniform sampler2D colorMap;
 
+  uniform bool enableIsoLines;
+  uniform float isoLineWidth;
+  uniform float isoLineFrequency;
+  uniform vec3 isoLineColor;
+
   void main() {
-    gl_FragColor = texture2D(colorMap, vUV);
+    float f = fract(height * isoLineFrequency);
+    // ignore very flat areas
+    f = max(0.0001, f);
+    float df = fwidth(height * isoLineFrequency);
+    float fstep = smoothstep(df, df * isoLineWidth, f);
+
+    // If isolines are disabled, force fstep to 1
+    fstep = mix(1.0, fstep, float(enableIsoLines));
+
+    vec4 textureColor = texture2D(colorMap, vUV);
+    gl_FragColor = mix(vec4(isoLineColor, 1.0), textureColor, fstep);
+
   }
 `;
 
@@ -39,6 +57,10 @@ export class HeightMapMaterial extends ShaderMaterial {
       colorMap: { value: this.colorTexture },
       heightMap: { value: this.heightMapTexture },
       heightScaleFactor: { value: 0.5 },
+      enableIsoLines: { value: false},
+      isoLineWidth: {value: 2.0 },
+      isoLineFrequency: {value: 100.0},
+      isoLineColor: {value: new Color(0.0, 0.5, 0.5)}
     };
     this.uniforms = uniforms;
   }
